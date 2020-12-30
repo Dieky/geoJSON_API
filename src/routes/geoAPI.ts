95dc43d92f163712fd33820d316f434aa1a55734
Patrick <test@test.com>
2020-12-30 02:19:25 +0100

    adding the code for geoAPI

--- src/routes/geoAPI.ts
+++ src/routes/geoAPI.ts
@@ -1,45 +1,69 @@
 import express from "express";
 import gameFacade from "../facades/gameFacade";
+import {gameArea, players } from "../config/gameData";
+const gju = require("geojson-utils")
 const router = express.Router();
 
 //Just to check this router is up and running
 router.get('/', async function (req, res, next) {
-  res.json({ msg: "game API" })
+    res.json(gameArea)
 })
 
-// router.post("/mobile", async function(req,res,next){
-//   try {
-//     let {username,lon,lat} = req.body;
-//     //Read the exercise and check what must be sent with the request. Grab this information from the request body, and 
-//     //call the method (the skeleton is already there) nearbyPlayers(....) in the gameFacade and send back the result to the client
-    
-//     const response = await gameFacade.updateLocationMobile(username,lon,lat)
-//     return res.json(response)
-//     /*
-//      */
-//   } catch (err) {
-//     next(err)
-//   }
-  
-// })
 
-// router.post('/nearbyplayers', async function (req, res, next) {
-//   try {
-//     let {username,password,lon,lat,radius} = req.body;
-//     //Read the exercise and check what must be sent with the request. Grab this information from the request body, and 
-//     //call the method (the skeleton is already there) nearbyPlayers(....) in the gameFacade and send back the result to the client
-    
-//     const response = await gameFacade.nearbyPlayers(username,password,lon,lat,radius)
-//     return res.json(response)
-//     /*
-//      */
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+let polygonForClient:any = {};
+polygonForClient.coordinates = gameArea.coordinates[0].map(point => {
+    return { latitude: point[1], longitude: point[0] }
+})
 
-// router.post('/getPostIfReached', async function (req, res, next) {
-//   throw new Error("Not yet implemented")
-// })
+//Returns a polygon, representing the gameArea
+router.get("/geoapi/gamearea", (req, res) => {
+    res.json(polygonForClient);
+});
+
+router.get('/geoapi/isuserinarea/:lon/:lat', function (req, res) {
+    const lon = req.params.lon;
+    const lat = req.params.lat;
+    const point = { "type": "Point", "coordinates": [lon, lat] }
+    let isInside = gju.pointInPolygon(point, gameArea);
+    let result:any = {};
+    result.status = isInside;
+    let msg = isInside ? "Point was inside the tested polygon" :
+        "Point was NOT inside tested polygon";
+    result.msg = msg;
+    res.json(result);
+});
+
+router.get('/geoapi/findNearbyPlayers/:lon/:lat/:rad', function (req, res) {
+    const lon = Number(req.params.lon);
+    const lat = Number(req.params.lat);
+    const rad = Number(req.params.rad);
+    const point = { "type": "Point", "coordinates": [lon, lat] }
+    let isInside = gju.pointInPolygon(point, gameArea);
+    let result:any = [];
+    players.forEach(player => {
+        if (gju.geometryWithinRadius(player.geometry, point, rad)) {
+            result.push(player);
+        }
+    })
+    res.json(result);
+});
+
+router.get('/geoapi/distanceToUser/:lon/:lat/:username', function (req, res) {
+    const { lon, lat, username } = req.params
+    const point = { "type": "Point", "coordinates": [Number(lon), Number(lat)] }
+    console.log(point, username);
+    const user = players.find((player) => {
+        return player.properties.name === username
+    });
+    if (!user) {
+        res.status(404);
+        return res.json({ msg: "User not found" });
+    }
+    let distance = gju.pointDistance(point, user.geometry);
+    let result = { distance: distance, to: username }
+    res.json(result);
+});
+
+
 
 module.exports = router;
